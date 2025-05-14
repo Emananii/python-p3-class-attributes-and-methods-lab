636f2a2fb48c205d006a7341136a9bf4ec9d3536
anita-that-chiq <62019551+kahenya-anita@users.noreply.github.com>
2025-05-14 10:06:44 +0300

    Adding to genres and songs

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,27 @@
 class Song:
-    pass
+    count = 0
+    artists = []
+    genres = []
+    genre_count = {}
+    artist_count = {}
+   
+    def __init__(self, artist, genre ):
+        self.artist = artist
+        self.genre = genre
+        
+    #Tracking song count
+    @classmethod
+    def add_song_to_count(cls):
+        cls.count += 1
+        
+    #Add a new genre to the genre list
+    @classmethod
+    def add_genre(cls, genre):
+        if genre not in cls.genres:
+            cls.genres.append(genre)
+            cls.genre_count[genre] = 1
+        else:
+            cls.genre_count[genre] += 1
+        
+   
+   
